5e8c43c6e60768b7618b9afde1be9921fee0d62f
Shikhar Agrahari <shikharagrahari2403@gmail.com>
2026-06-12 12:22:06 +0530

    Add files via upload

---
+++ evaluations/TM_modules/extraction.py
@@ -0,0 +1,178 @@
+import subprocess
+import numpy as np
+from Bio.PDB import PDBParser
+
+def get_masses_and_coords(
+    pdb_file
+):
+
+    parser = PDBParser(
+        QUIET=True
+    )
+
+    structure = parser.get_structure(
+        "structure",
+        pdb_file
+    )
+
+    masses = []
+    coords = []
+
+    for atom in structure.get_atoms():
+
+        masses.append(
+            atom.mass
+        )
+
+        coords.append(
+            atom.coord
+        )
+
+    return (
+
+        np.array(masses),
+
+        np.array(coords)
+
+    )
+
+
+def get_radius_of_gyration(
+    pdb_file
+):
+
+    masses, coords = (
+        get_masses_and_coords(
+            pdb_file
+        )
+    )
+
+    total_mass = np.sum(
+        masses
+    )
+
+    weighted_coords = (
+        coords.T * masses
+    )
+
+    center_of_mass = (
+
+        np.sum(
+            weighted_coords,
+            axis=1
+        )
+
+        /
+
+        total_mass
+
+    )
+
+    squared_distances = np.sum(
+
+        (coords - center_of_mass) ** 2,
+
+        axis=1
+
+    )
+
+    rg = np.sqrt(
+
+        np.sum(
+            squared_distances * masses
+        )
+
+        /
+
+        total_mass
+
+    )
+
+    return float(
+        rg
+    )
+
+
+def get_extraction_diameter(
+    pdb_file
+):
+
+    rg = get_radius_of_gyration(
+        pdb_file
+    )
+
+    return int(
+        round(
+            2 * rg
+        )
+    )
+
+
+def build_extraction_command(
+    job_file,
+    config,
+    extraction_particle_diameter,
+    ignore_tomogram_mask = False
+):
+
+    cmd = [
+
+        "pytom_extract_candidates.py",
+
+        "-j",
+        job_file,
+
+        "-n",
+        str(
+            config["extraction"]
+                  ["number_of_particles"]
+        )
+
+    ]
+
+    cutoff = (
+        config["extraction"]
+              ["cutoff"]
+    )
+
+    cmd.extend([
+
+            "--particle-diameter",
+
+            str(extraction_particle_diameter)
+
+    ])
+
+    if cutoff is not None:
+
+        cmd.extend([
+
+            "-c",
+
+            str(cutoff)
+
+        ])
+
+
+    if ignore_tomogram_mask:
+        cmd.append(
+            "--ignore_tomogram_mask"
+        )
+
+    return cmd
+
+
+def run_extraction_command(cmd):
+
+    print(
+        "\nRunning Extraction:\n"
+    )
+
+    print(
+        " ".join(cmd)
+    )
+
+    subprocess.run(
+        cmd,
+        check=True
+    )
