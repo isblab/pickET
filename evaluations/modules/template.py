6e3dc8e7b6ed62a457f64d4ea12fe629c951b8e1
Shikhar Agrahari <shikharagrahari2403@gmail.com>
2026-06-12 11:32:55 +0530

    Update template.py

--- evaluations/modules/template.py
+++ evaluations/modules/template.py
@@ -48,40 +48,6 @@ def get_atom_coordinates(
         coordinates
     )
 
-def get_masses_and_coords(
-    pdb_file
-):
-
-    parser = PDBParser(
-        QUIET=True
-    )
-
-    structure = parser.get_structure(
-        "structure",
-        pdb_file
-    )
-
-    masses = []
-    coords = []
-
-    for atom in structure.get_atoms():
-
-        masses.append(
-            atom.mass
-        )
-
-        coords.append(
-            atom.coord
-        )
-
-    return (
-
-        np.array(masses),
-
-        np.array(coords)
-
-    )
-
 def estimate_diameter_from_pdb(
     pdb_file
 ):
@@ -142,69 +108,6 @@ def get_particle_diameter(
     return int(
         round(
             diameter
-        )
-    )
-
-
-def get_radius_of_gyration(
-    pdb_file
-):
-
-    masses, coords = (
-        get_masses_and_coords(
-            pdb_file
-        )
-    )
-
-    total_mass = np.sum(
-        masses
-    )
-
-    weighted_coords = (
-        coords.T * masses
-    )
-
-    center_of_mass = (
-        np.sum(
-            weighted_coords,
-            axis=1
-        )
-        /
-        total_mass
-    )
-
-    squared_distances = np.sum(
-
-        (coords - center_of_mass) ** 2,
-
-        axis=1
-
-    )
-
-    rg = np.sqrt(
-
-        np.sum(
-            squared_distances * masses
-        )
-        /
-        total_mass
-
-    )
-
-    return float(rg)
-
-
-def get_extraction_diameter(
-    pdb_file
-):
-
-    rg = get_radius_of_gyration(
-        pdb_file
-    )
-
-    return int(
-        round(
-            2 * rg
         )
     )
 
