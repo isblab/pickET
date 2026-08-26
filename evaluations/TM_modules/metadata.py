442c2dc24775ec996dc370cb05d7db5c72576293
omgol411 <omgolatkar2001@gmail.com>
2026-08-26 18:26:28 +0530

    bug fix: decide mask name based on tomotwin sim round tomogram number

--- evaluations/TM_modules/metadata.py
+++ evaluations/TM_modules/metadata.py
@@ -2,10 +2,30 @@ import os
 import mrcfile
 
 
-def fix_tutorial_voxel_headers(tomogram_files):
-    
+def get_result_dirname(
+    dataset_type: str,
+    tomogram_path: str,
+) -> str:
+
+    if dataset_type == "simulated":
+        basename = os.path.basename(os.path.dirname(tomogram_path))
+
+    elif dataset_type in ["experimental", "tutorial"]:
+        basename = os.path.splitext(os.path.basename(tomogram_path))[0]
+
+    else:
+        raise ValueError(
+            f"Invalid parameter dataset_type: {dataset_type}"
+            "Valid options: 'experimental' or 'simulated' or 'tutorial'"
+        )
+
+    return basename
+
+
+def fix_tutorial_voxel_headers(tomogram_files: list):
+
     for fpath in tomogram_files:
-        
+
         if not (fpath.endswith(".mrc")):
             continue
 
@@ -14,25 +34,7 @@ def fix_tutorial_voxel_headers(tomogram_files):
             mrc.voxel_size = 13.79
 
 
-def get_voxel_size(mrc_path):
-
-    with mrcfile.open(mrc_path, permissive=True) as mrc:
-
-        voxel_size = float(mrc.voxel_size.x)
-
-    return voxel_size
-
-
-def get_tomogram_shape(mrc_path):
-
-    with mrcfile.open(mrc_path, permissive=True) as mrc:
-
-        shape = mrc.data.shape
-
-    return shape
-
-
-def discover_sidecar_files(mrc_path):
+def discover_sidecar_files(mrc_path: str) -> list:
 
     base = os.path.splitext(mrc_path)[0]
 
@@ -45,42 +47,47 @@ def discover_sidecar_files(mrc_path):
     return files
 
 
-def get_metadata(tomogram_files, config):
+def get_metadata(
+    tomogram_files: list,
+    config: dict,
+) -> list:
 
     if config["dataset"]["type"] == "tutorial":
 
-        print(
-            "\nApplying tutorial "
-            "voxel-size correction..."
-        )
+        print("\nApplying tutorial " "voxel-size correction...")
 
-        fix_tutorial_voxel_headers(tomogram_files)
+        fix_tutorial_voxel_headers(tomogram_files=tomogram_files)
 
     dataset = []
 
-    # ----------------------------------
-    # Tomogram discovery
-    # ----------------------------------
+    for _, mrc_file in enumerate(tomogram_files):
 
-    for idx, mrc_file in enumerate(tomogram_files):
+        result_name = get_result_dirname(
+            dataset_type=config["dataset"]["type"],
+            tomogram_path=mrc_file
+        )
 
-        basename = os.path.splitext(os.path.basename(mrc_file))[0]
+        tomo_cfg = config.get("tomograms", {}).get(result_name, {})
 
-        tomo_cfg = config.get("tomograms", {}).get(basename, {})
-
-        unknown = set(tomo_cfg.keys()) - {"picket_segmentation", "invert_mask", "tilt_range"}
+        unknown = set(tomo_cfg.keys()) - {
+            "picket_segmentation",
+            "invert_mask",
+            "tilt_range",
+        }
 
         if unknown:
 
-            raise ValueError(f"{basename}: unknown tomogram keys {unknown}")
+            raise ValueError(f"{result_name}: unknown tomogram keys {unknown}")
 
-        mask_path = os.path.join(config["preprocessing"]["picket_out_mrc"], f"{basename}_mask.mrc")
+        mask_path = os.path.join(
+            config["preprocessing"]["picket_out_mrc"], f"{result_name}_mask.mrc"
+        )
 
-        if not os.path.exists(mask_path):
+        # if not os.path.exists(mask_path):
 
-            raise FileNotFoundError(f"Mask not found: {mask_path}")
+        #     raise FileNotFoundError(f"Mask not found: {mask_path}")
 
-        sidecars = discover_sidecar_files(mrc_file)
+        sidecars = discover_sidecar_files(mrc_path=mrc_file)
 
         # ----------------------------------
         # Tilt-angle handling
@@ -93,7 +100,9 @@ def get_metadata(tomogram_files, config):
         elif "tilt_range" in tomo_cfg:
 
             if len(tomo_cfg["tilt_range"]) != 2:
-                raise ValueError(f"{basename}: tilt_range must contain [min, max]")
+                raise ValueError(
+                    f"{result_name}: tilt_range must contain [min, max]"
+                )
 
             min_tilt = tomo_cfg["tilt_range"][0]
             max_tilt = tomo_cfg["tilt_range"][1]
@@ -103,22 +112,19 @@ def get_metadata(tomogram_files, config):
             min_tilt = config["tilt_angles"]["min"]
             max_tilt = config["tilt_angles"]["max"]
 
+
+        with mrcfile.open(mrc_file, permissive=True) as mrc:
+            voxel_size = float(mrc.voxel_size.x)
+            mrc_shape = mrc.data.shape
+
         tomogram = {
-
             "path": mrc_file,
-
-            "shape": get_tomogram_shape(mrc_file),
-
-            "voxel_size": get_voxel_size(mrc_file),
-
+            "shape": mrc_shape,
+            "voxel_size": voxel_size,
             "rawtlt": sidecars["rawtlt"],
-
             "mask_path": mask_path,
-
             "min_tilt": min_tilt,
-
-            "max_tilt": max_tilt
-
+            "max_tilt": max_tilt,
         }
 
         dataset.append(tomogram)
--- evaluations/TM_modules/preprocessing.py
+++ evaluations/TM_modules/preprocessing.py
@@ -6,12 +6,17 @@ import mrcfile
 import numpy as np
 import re
 
+from TM_modules.metadata import get_result_dirname
+
+
 def numeric_key(path):
     filename = os.path.basename(path)
     return [
-        int(x) if x.isdigit() else x
-        for x in re.split(r'(\d+)', filename)
+        int(x)
+        if x.isdigit()
+        else x for x in re.split(r"(\d+)", filename)
     ]
+
 
 def load_h5_mask(h5_file):
 
@@ -19,54 +24,91 @@ def load_h5_mask(h5_file):
         seg = f["segmentations"]["semantic_segmentation"][:]
     return seg
 
+
 def convert_to_binary_mask(seg):
 
     return (seg == 1).astype(np.uint8)
+
 
 def invert_mask(mask):
 
     return (1 - mask).astype(np.uint8)
 
+
 def compute_occupancy(mask):
 
     return float(100 * np.mean(mask > 0))
+
 
 def save_mrc_mask(mask, output_path):
 
     with mrcfile.new(output_path, overwrite=True) as m:
         m.set_data(mask)
 
-def get_tomogram_files(tomogram_folder, dataset="experimental"):
 
-    if dataset=="experimental":
+def get_tomogram_files(
+    tomogram_folder: str,
+    dataset_type: str = "experimental"
+):
+    """ Discover tomograms in a given directory.
+
+    Args:
+        tomogram_folder (str): Directory to search for tomograms
+        dataset_type (str, optional): Type of dataset
+            "simulated" or "experimental" or "tutorial".
+            Defaults to "experimental".
+
+    Returns:
+        list: List of discovered tomogram paths
+    """
+
+    if dataset_type in ["experimental", "tutorial"]:
         tomogram_files = sorted(
-            glob.glob(os.path.join(tomogram_folder, "*.mrc")), key=numeric_key
+            glob.glob(os.path.join(tomogram_folder, "*.mrc")),
+            key=numeric_key
         )
-    elif dataset=="simulated":
-        tomogram_files = glob.glob(os.path.join(tomogram_folder, "**", "*.mrc"), recursive=True)
+    elif dataset_type == "simulated": # tomotwin
+        tomogram_files = glob.glob(
+            os.path.join(tomogram_folder, "**", "*.mrc"),
+            recursive=True
+        )
         tomogram_files = sorted([
-            f for f in tomogram_files if f.endswith("denoised_tiltseries_rec.mrc")
+            f for f in tomogram_files
+            if os.path.basename(f) == "tiltseries_rec.mrc"
         ])
     else:
         raise ValueError(
-            f"Invalid parameter dataset: {dataset}"
-            "Valid options: 'experimental' or 'simulated'"
+            f"Invalid parameter dataset_type: {dataset_type}"
+            "Valid options: 'experimental' or 'simulated' or 'tutorial'"
         )
 
     return tomogram_files
 
-def run_preprocessing(
-    tomogram_files,
-    picket_in_h5,
-    picket_out_mrc,
-    tomogram_config=None
-):
 
-    if tomogram_config is None:
-        tomogram_config = {}
+def run_preprocessing(
+    tomogram_files: list,
+    picket_in_h5: str,
+    picket_out_mrc: str,
+    tomogram_config: dict = {},
+    dataset_type: str = "experimental",
+):
+    """ Preprocesing steps:
+    - Convert h5 picket semantic segmenations to mrc format for each tomogram
+    - Compute per-tomogram occupancy and save a summary file
+
+    Args:
+        tomogram_files (list): List of full paths to a set of tomograms
+        picket_in_h5 (str): Picket semantic segmentation directory with *.h5 files
+        picket_out_mrc (str): Output directory to store converted mrc segmentations
+        tomogram_config (dict, optional): Parameters specific to each
+            tomogram if any. Defaults to {}.
+    """
+
     os.makedirs(picket_out_mrc, exist_ok=True)
+
     segmentation_files = sorted(
-        glob.glob(os.path.join(picket_in_h5, "*.h5")), key=numeric_key
+        glob.glob(os.path.join(picket_in_h5, "*.h5")),
+        key=numeric_key
     )
     if len(segmentation_files) != len(tomogram_files):
         raise ValueError("Mismatch between tomograms and segmentations.")
@@ -74,14 +116,19 @@ def run_preprocessing(
     occupancy_summary = {}
 
     seg_iter = iter(segmentation_files)
+
     for tomo_file in tomogram_files:
-        tomo_name = os.path.splitext(os.path.basename(tomo_file))[0]
+
+        # tomo_name = os.path.splitext(os.path.basename(tomo_file))[0]
+        tomo_name = get_result_dirname(
+            dataset_type=dataset_type,
+            tomogram_path=tomo_file,
+        )
         tomo_cfg = tomogram_config.get(tomo_name, {})
+        output_path = os.path.join(picket_out_mrc, f"{tomo_name}_mask.mrc")
+
         if "picket_segmentation" in tomo_cfg:
-            h5_file = os.path.join(
-                picket_in_h5,
-                tomo_cfg["picket_segmentation"]
-            )
+            h5_file = os.path.join(picket_in_h5, tomo_cfg["picket_segmentation"])
             if not os.path.exists(h5_file):
                 raise FileNotFoundError(
                     f"Segmentation file not found for {tomo_name}: {h5_file}"
@@ -89,32 +136,33 @@ def run_preprocessing(
         else:
             h5_file = next(seg_iter)
         print(f"{tomo_name} <-- {os.path.basename(h5_file)}")
+
         seg = load_h5_mask(h5_file)
         mask = convert_to_binary_mask(seg)
         inverted = False
         if tomo_cfg.get("invert_mask", False):
             mask = invert_mask(mask)
             inverted = True
+
         occupancy = compute_occupancy(mask)
-        output_path = os.path.join(picket_out_mrc, f"{tomo_name}_mask.mrc")
         occupancy_summary[tomo_name] = {
             "occupancy_percent": round(occupancy, 2),
             "inverted": inverted,
-            "segmentation_file": os.path.basename(h5_file)
+            "segmentation_file": os.path.basename(h5_file),
         }
         print(
-            f"{tomo_name}: "
-            f"{occupancy:.2f}% "
-            f"{'(inverted)' if inverted else ''}"
+            f"{tomo_name}: " f"{occupancy:.2f}% " f"{'(inverted)' if inverted else ''}"
         )
 
         if os.path.exists(output_path):
             print(f"Skipping existing mask: {tomo_name}")
             continue
+
         save_mrc_mask(mask, output_path)
 
     summary_path = os.path.join(picket_out_mrc, "occupancy_summary.yaml")
     with open(summary_path, "w") as f:
         yaml.dump(occupancy_summary, f, sort_keys=False)
+
     print("\nSaved occupancy summary:")
     print(summary_path)
